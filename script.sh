c30eef7bde856310e873a8bfa9986a142a85bf55
Duong <pduongpdu99@gmail.com>
2026-04-06 08:08:27 +0700

    clone source

---
+++ script.sh
@@ -0,0 +1,9 @@
+// add all items to local commit
+// Being able to add specific folder or file
+// "." letter mean ALL unstaged and modified item
+git add .
+
+// require: write down a message to describe this commit
+git commit -m "describe this commit"
+
+git push
